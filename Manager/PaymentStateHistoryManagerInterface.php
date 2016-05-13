3449ddb547093505dd7a7db05a571e0d6a851253
Adam Piotrowski <adam@wellcommerce.org>
2016-05-13 10:32:44 +0200

    Preparations before next beta

--- Manager/PaymentStateHistoryManagerInterface.php
+++ Manager/PaymentStateHistoryManagerInterface.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * WellCommerce Open-Source E-Commerce Platform
- * 
+ *
  * This file is part of the WellCommerce package.
  *
  * (c) Adam Piotrowski <adam@wellcommerce.org>
- * 
+ *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
@@ -13,17 +13,15 @@
 namespace WellCommerce\Bundle\PaymentBundle\Manager;
 
 use WellCommerce\Bundle\DoctrineBundle\Manager\ManagerInterface;
-use WellCommerce\Bundle\OrderBundle\Entity\OrderInterface;
 use WellCommerce\Bundle\PaymentBundle\Entity\PaymentInterface;
+use WellCommerce\Bundle\PaymentBundle\Entity\PaymentStateHistoryInterface;
 
 /**
- * Interface PaymentManagerInterface
+ * Interface PaymentStateHistoryManagerInterface
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-interface PaymentManagerInterface extends ManagerInterface
+interface PaymentStateHistoryManagerInterface extends ManagerInterface
 {
-    public function createPaymentForOrder(OrderInterface $order) : PaymentInterface;
-
-    public function findPaymentByToken(string $token) : PaymentInterface;
+    public function createPaymentStateHistory(PaymentInterface $payment) : PaymentStateHistoryInterface;
 }
